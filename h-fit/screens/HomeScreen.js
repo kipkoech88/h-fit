b04440eddc80be08c8cc99cf58136073447c1fe4
kipkoech88 <hoseakipkoech88@gmail.com>
2023-07-30 19:00:08 +0300

    updated paywall screen

--- h-fit/screens/HomeScreen.js
+++ h-fit/screens/HomeScreen.js
@@ -47,7 +47,6 @@ export default function HomeScreen() {
         title="Connect with Friends"
         icon="share-social"
         screen="Demo"
-        vertical
         color="lightblue"
       />
       {/*actionrow */}
--- h-fit/screens/PaywallScreen.js
+++ h-fit/screens/PaywallScreen.js
@@ -3,6 +3,7 @@ import React from 'react'
 import tw from 'twrnc'
 import Ionicons from '@expo/vector-icons/Ionicons'
 import { useNavigation } from '@react-navigation/native'
+import {MaterialCommunityIcons} from '@expo/vector-icons'
 
 export default function PaywallScreen() {
     const navigation = useNavigation()
@@ -18,9 +19,49 @@ export default function PaywallScreen() {
         </View>
         <TouchableOpacity
         onPress={()=>navigation.goBack()}
+        style={tw`top-0 absolute right-0 p-5`}
         >
             <Ionicons name="md-close-circle-sharp" size={32} color="#E5962D"/>
         </TouchableOpacity>
+
+        {/*logo*/}
+        <View style={tw`items-center`}>
+            <MaterialCommunityIcons
+                name='trophy-award'
+                size={150}
+                color="orange"
+            />
+        </View>
+
+        <View style={tw`px-10 pt-5 space-y-5 pb-5`}>
+            <View style={tw`flex-row space-x-10 items-center`}>
+                <Ionicons name='md-key' size={32} color="orange"/>
+                <View style={tw`flex-1`}>
+                    <Text style={tw`text-white text-lg font-bold`}>Access to all Pro Features</Text>
+                    <Text style={tw`text-white text-sm font-extralight`}>
+                        Upgrade to the premium version of the app to access all features available 
+                    </Text>
+                </View>
+            </View>
+            <View style={tw`flex-row space-x-10 items-center`}>
+                <Ionicons name='md-person-add-outline' size={32} color="orange"/>
+                <View style={tw`flex-1`}>
+                    <Text style={tw`text-white text-lg font-bold`}>Helpline 24/7 to professional trainers</Text>
+                    <Text style={tw`text-white text-sm font-extralight`}>
+                        Get unlimited support to our fitness team at any time 
+                    </Text>
+                </View>
+            </View>
+            <View style={tw`flex-row space-x-10 items-center`}>
+                <Ionicons name='md-star' size={32} color="orange"/>
+                <View style={tw`flex-1`}>
+                    <Text style={tw`text-white text-lg font-bold`}>Unlock unlimited edition</Text>
+                    <Text style={tw`text-white text-sm font-extralight`}>
+                        Upgrade to the premium version of the app to access all features available 
+                    </Text>
+                </View>
+            </View>
+        </View>
     </ScrollView>
   )
 }
