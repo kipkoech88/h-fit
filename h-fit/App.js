97ad30b03da9014863a6428e2dc88b3593e7a2d8
kipkoech88 <hoseakipkoech88@gmail.com>
2023-07-30 16:56:27 +0300

    addupdated homescreen

--- h-fit/App.js
+++ h-fit/App.js
@@ -7,15 +7,15 @@ import LoginScreen from './screens/LoginScreen'
 import RegisterScreen from './screens/RegisterScreen'
 import HomeScreen from './screens/HomeScreen'
 
-const Stack = createNativeStackNavigator;
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen}/>
         <Stack.Screen name="Login" component={LoginScreen}/>
         <Stack.Screen name="Register" component={RegisterScreen}/>
-        <Stack.Screen name="Home" component={HomeScreen}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
--- h-fit/screens/HomeScreen.js
+++ h-fit/screens/HomeScreen.js
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, TouchableOpacity, SafeAreaView, ScrollView } from 'react-native'
 import React from 'react'
+import tw from 'twrnc'
+import { Touchable } from 'react-native';
+import {Ionicons} from '@expo/vector-icons'
 
 export default function HomeScreen() {
   return (
-    <View>
+    <SafeAreaView style={tw`relative flex-1`}>
+    <ScrollView>
       <Text>HomeScreen</Text>
-    </View>
+      {/* PRO/Upgrade */}
+      <TouchableOpacity style={tw`absolute z-50 top-5 right-5 items-center`}>
+        <Ionicons name="person-circle" size={32} color="orange" />
+        <Text style={tw``}>PRO/Upgrade</Text>
+      </TouchableOpacity>
+      {/* image */}
+      <Image
+        style={tw`w-full h-64`}
+        source={{uri: "https://www.shutterstock.com/shutterstock/photos/1663692082/display_1500/stock-vector-fitness-gym-logo-design-template-with-exercising-athletic-man-and-woman-isolated-on-white-vector-1663692082.jpg"}}
+      />
+      {/* actionrow */}
+      {/*actionrow */}
+      {/*actionrow */}
+      </ScrollView>
+
+    </SafeAreaView>
   )
 }
