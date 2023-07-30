54719564706fd741849d378e01c89486b349b5b2
kipkoech88 <hoseakipkoech88@gmail.com>
2023-07-30 18:38:07 +0300

    added paywall navigation

--- h-fit/screens/PaywallScreen.js
+++ h-fit/screens/PaywallScreen.js
@@ -1,10 +1,26 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
 import React from 'react'
+import tw from 'twrnc'
+import Ionicons from '@expo/vector-icons/Ionicons'
+import { useNavigation } from '@react-navigation/native'
 
 export default function PaywallScreen() {
+    const navigation = useNavigation()
   return (
-    <View>
-      <Text>PaywallScreen</Text>
-    </View>
+    <ScrollView style={tw`bg-blue-300 flex-1`}>
+        <View style={tw`m-10 py-2`}>
+                <Text style={tw`uppercase font-bold text-xl items-center text-center text-white`}>
+                    Upgrade
+                </Text>
+                <Text style={tw`text-white text-center`}>
+                    Upgrade to pro to access all features
+                </Text>
+        </View>
+        <TouchableOpacity
+        onPress={()=>navigation.goBack()}
+        >
+            <Ionicons name="md-close-circle-sharp" size={32} color="#E5962D"/>
+        </TouchableOpacity>
+    </ScrollView>
   )
 }
